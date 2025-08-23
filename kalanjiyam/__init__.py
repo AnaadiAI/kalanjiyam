e0bfd9dd50ba87b259183f9efcf9128998c7f2d1
venkatapathy <venkat.s.iyer@gmail.com>
2025-08-23 10:59:29 +0530

    Task 3: "Creating a viewing page where anyone(without logging) can view ocred and translated books. 1. It should have a page to list all the books 2. when a books is selected to will go to page view where the ocr and image(and toggling translation) can be viewed. No menu, etc "

--- kalanjiyam/__init__.py
+++ kalanjiyam/__init__.py
@@ -30,6 +30,7 @@ from kalanjiyam.views.auth import bp as auth
 from kalanjiyam.views.blog import bp as blog
 from kalanjiyam.views.dictionaries import bp as dictionaries
 from kalanjiyam.views.proofing import bp as proofing
+from kalanjiyam.views.public import bp as public
 from kalanjiyam.views.reader.parses import bp as parses
 from kalanjiyam.views.reader.texts import bp as texts
 from kalanjiyam.views.site import bp as site
@@ -138,6 +139,7 @@ def create_app(config_env: str):
     app.register_blueprint(dictionaries, url_prefix=f"{url_prefix}/tools/dictionaries")
     app.register_blueprint(parses, url_prefix=f"{url_prefix}/parses")
     app.register_blueprint(proofing, url_prefix=f"{url_prefix}/proofing")
+    app.register_blueprint(public, url_prefix=f"{url_prefix}/books")
     app.register_blueprint(site, url_prefix=url_prefix)
     app.register_blueprint(texts, url_prefix=f"{url_prefix}/texts")
 
